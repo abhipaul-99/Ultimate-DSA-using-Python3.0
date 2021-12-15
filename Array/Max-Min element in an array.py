f525f8e94d84f8d6d8852066e9734df208b09f05
abhipaul-99 <reachtoabhishekpaul@gmail.com>
2021-12-16 00:44:09 +0530

    Update Max-Min element in an array.py

--- Array/Max-Min element in an array.py
+++ Array/Max-Min element in an array.py
@@ -1,3 +1,4 @@
+#Using Iterative method
 def maxmin(arr):
     mi = arr[0]
     ma = arr[0]
@@ -10,6 +11,27 @@ def maxmin(arr):
     print("Min:",mi)
     print("Max:",ma)
 
+
+
+arr = [5,8,2,6,9,99,14,23]
+maxmin(arr)
+
+
+
+
+#Using Comparison
+def maxmin(arr):
+    maax=0
+    miin = 0
+    for i in range(len(arr)-1):
+        z=max(arr[i],arr[i+1])
+        if maax<z:
+            maax=z
+        x = min(arr[i],arr[i+1])
+        if miin>x:
+            miin=x
+    print("Min:",miin)
+    print("Max:",maax)
 
 
 arr = [5,8,2,6,9,99,14,23]
